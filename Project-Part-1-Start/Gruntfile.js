976ec1a10e0a3f67d3ecbf8752cc99e2b847864d
Amr <amr.e.dev@gmail.com>
2015-07-10 09:06:18 +0200

    changed image quality in grunt

--- Project-Part-1-Start/Gruntfile.js
+++ Project-Part-1-Start/Gruntfile.js
@@ -14,13 +14,11 @@ module.exports = function(grunt) {
         options: {
           engine: 'im',
           sizes: [{
-            /*
-            Change these:
             
-            width: ,
-            suffix: ,
-            quality:
-            */
+            width: 640,
+            suffix: "_x2",
+            quality: 0.6
+
           }]
         },
 
