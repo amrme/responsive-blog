8c5df967d28a8f983a5ad5ab640e09a5daa77ea1
Amr <amr.e.dev@gmail.com>
2015-07-10 09:24:31 +0200

    still failing

--- Project-Part-1-Start/Gruntfile.js
+++ Project-Part-1-Start/Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
           sizes: [{
             
             width: '100%',
-            suffix: "none",
+            suffix: '',
             quality: 100
 
           }]
